9d94058d9cf4c959803cff4492715de5c7f4ff8b
Alexander Fisher <58009009+alexfishy12@users.noreply.github.com>
2025-07-27 00:45:21 -0400

    Few tweaks

--- PlayerListItem.cs
+++ PlayerListItem.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class PlayerrListItem : PanelContainer
+public partial class PlayerListItem : PanelContainer
 {
     [Export] private Label nameLabel;
     [Export] private Label pingLabel;
